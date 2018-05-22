bc7addb40205caaa3af055e2dd9d02618e38c498
Tony Cassara <acassara@gmail.com>
2018-05-22 08:06:05 -0700

    fix: portfolio link

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const MenuBar = () => (
   <div id="menu-bar">
-    <h6><Link to="/">Portfolio</Link></h6>
+    <h6><Link to="/portfolio">Portfolio</Link></h6>
     <h6><Link to="/aboutme">About Me</Link></h6>
     {/* <h6><Link to="/contact">Contact</Link></h6> */}
   </div>
