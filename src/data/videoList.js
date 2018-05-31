cb4b0d2e18754350139c3451cdc3deaff512eabf
Tony Cassara <acassara@gmail.com>
2018-05-31 08:32:57 -0700

    fix: videolist

--- src/data/videoList.js
+++ src/data/videoList.js
@@ -2,18 +2,18 @@ import formatYouTubeLink from '../helpers/formatYouTubeLink'
 
 const videoUrls = [
   // 'https://www.youtube.com/watch?v=0wJIDTC6ddk',
-  'https://www.youtube.com/watch?v=hUklgKPMe1I',
   // 'https://www.youtube.com/watch?v=CUf9ioY19sU',
-  'https://www.youtube.com/watch?v=qgtIQisXQQM',
-  'https://www.youtube.com/watch?v=rHa6qDSAr8M',
-  'https://www.youtube.com/watch?v=m-HpS9g3PjM',
-  'https://www.youtube.com/watch?v=3UQukGMqoPY',
-  'https://www.youtube.com/watch?v=eTShf6EtQR0',
-  'https://www.youtube.com/watch?v=LLd7LgcOYQE',
-  'https://www.youtube.com/watch?v=cU9AM7gF7qQ',
-  'https://www.youtube.com/watch?v=xiYpKpV1LsY',
-  'https://www.youtube.com/watch?v=KgY-eexa5Lo',
-  'https://www.youtube.com/watch?v=M4W7yjqL8Ks',
+  'https://www.youtube.com/watch?v=hUklgKPMe1I', // full frame point shoot
+  'https://www.youtube.com/watch?v=qgtIQisXQQM', // my evening routine
+  'https://www.youtube.com/watch?v=rHa6qDSAr8M', // hair straightening
+  'https://www.youtube.com/watch?v=m-HpS9g3PjM', // shopping haul
+  'https://www.youtube.com/watch?v=3UQukGMqoPY', // sf apt tour
+  'https://www.youtube.com/watch?v=eTShf6EtQR0', // morning routine
+  'https://www.youtube.com/watch?v=LLd7LgcOYQE', // boyfriend does my vo
+  'https://www.youtube.com/watch?v=cU9AM7gF7qQ', // pancakes
+  'https://www.youtube.com/watch?v=xiYpKpV1LsY', // bose vs sony headphones
+  'https://www.youtube.com/watch?v=KgY-eexa5Lo', // pop socket review
+  'https://www.youtube.com/watch?v=M4W7yjqL8Ks', // coding bootcamp
 ]
 
 const videoList = videoUrls.map(vid => formatYouTubeLink(vid))
