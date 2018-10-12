2aab41e7abc40958fcabc0018d3c0fb75430aae3
Tony Cassara <acassara@gmail.com>
2018-10-11 20:14:22 -0700

    fix: update video list

--- src/data/videoList.js
+++ src/data/videoList.js
@@ -7,12 +7,14 @@ const videoUrls = [
   'https://www.youtube.com/watch?v=qgtIQisXQQM', // my evening routine
   'https://www.youtube.com/watch?v=4Y1v9heACD8', // how to find a job
   'https://www.youtube.com/watch?v=3UQukGMqoPY', // sf apt tour
-  'https://www.youtube.com/watch?v=hUklgKPMe1I', // full frame point shoot
+  'https://www.youtube.com/watch?v=ounWODHg9bs', // lasik surgery
   'https://www.youtube.com/watch?v=LLd7LgcOYQE', // boyfriend does my vo
-  'https://www.youtube.com/watch?v=M4W7yjqL8Ks', // coding bootcamp
-  'https://www.youtube.com/watch?v=oxwdWAqlLE0', // haul bellelily
-  'https://www.youtube.com/watch?v=xiYpKpV1LsY', // bose vs sony headphones
-  'https://www.youtube.com/watch?v=m-HpS9g3PjM', // haul slippers
+  'https://www.youtube.com/watch?v=ewVQqRZgg58', // why i left youtube
+  // 'https://www.youtube.com/watch?v=hUklgKPMe1I', // full frame point shoot
+  // 'https://www.youtube.com/watch?v=M4W7yjqL8Ks', // coding bootcamp
+  // 'https://www.youtube.com/watch?v=oxwdWAqlLE0', // haul bellelily
+  // 'https://www.youtube.com/watch?v=xiYpKpV1LsY', // bose vs sony headphones
+  // 'https://www.youtube.com/watch?v=m-HpS9g3PjM', // haul slippers
 ]
 
 const videoList = videoUrls.map(vid => formatYouTubeLink(vid))
