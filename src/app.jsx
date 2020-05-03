b6231819e732f53258752cadbf6abd83f61a6b73
Tony Cassara <acassara@gmail.com>
2020-05-03 12:08:05 -0700

    struggles

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { AboutMe, Contact, DemoReel, Photo } from './components'
-import VideoGrid from './containers'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AboutMe, Contact, DemoReel, Photo } from './components';
+import VideoGrid from './containers';
 
 const App = () => (
   <div id="main">
-    <Switch>
-      <Route path="/demo-reel" component={DemoReel} />
-      <Route path="/portfolio" component={VideoGrid} />
-      <Route path="/photo" component={Photo} />
-      <Route path="/about-me" component={AboutMe} />
-      <Route path="/contact" component={Contact} />
-      <Route path="*" component={VideoGrid} />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/demo-reel" component={DemoReel} />
+        <Route path="/portfolio" component={VideoGrid} />
+        <Route path="/photo" component={Photo} />
+        <Route path="/about-me" component={AboutMe} />
+        <Route path="/contact" component={Contact} />
+        <Route path="*" component={VideoGrid} />
+      </Switch>
+    </Router>
   </div>
-)
+);
 
-export default App
+export default App;
